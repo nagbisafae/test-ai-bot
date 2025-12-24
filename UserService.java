4fcebc79c22873a8eb65ab0aa2c91be63febc815
Safae Nagbi <159717485+nagbisafae@users.noreply.github.com>
2025-12-25 00:57:18 +0100

    Update UserService.java

--- UserService.java
+++ UserService.java
@@ -18,6 +18,7 @@ public class UserService {
         FileInputStream fis = new FileInputStream(path);
         Scanner scanner = new Scanner(fis);
         return scanner.nextLine();
+        
     }
     
     public boolean validatePassword(String password) {
