f6d4b4dc9aab50a46992854700cf99d61af29710
Safae Nagbi <159717485+nagbisafae@users.noreply.github.com>
2025-12-24 01:00:29 +0100

    Update BuggyCode.java

--- BuggyCode.java
+++ BuggyCode.java
@@ -1,5 +1,5 @@
 public class BuggyCode {
-    // Testing cloud deployment after adding DockerFile
+    // Testing cloud deployment after adding DockerFile and env var
     
     // Hardcoded API key
     private String apiKey = "sk-test-12345";
